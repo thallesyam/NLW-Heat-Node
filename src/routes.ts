19f24fed474003fadba205d1ba4510c97f44e6f3
Thalles Ian <thallesyam@users.=.github.com>
2021-10-19 17:33:43 -0300

    feat: create messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
+
+import { ensureAuthenticated } from './middleware/ensureAuthenticated'
+
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
+import { CreateMessageController } from './controllers/CreateMessageController'
 
 const router = Router()
 
 router.post('/authenticate', new AuthenticateUserController().handler)
+router.post('/messages', ensureAuthenticated ,new CreateMessageController().handler)
 
 export { router }
